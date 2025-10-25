d366e67b74f579ffbe426601c1ce89113362caf3
lleen <mygoldencode@gmail.com>
2025-10-25 12:21:03 +0500

    AddFinalRealese

---
+++ QazaqGeoReports/QazaqGeoReports.Infrastructure/Migrations/20251025011825_FinalRelaese.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QazaqGeoReports.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalRelaese : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
