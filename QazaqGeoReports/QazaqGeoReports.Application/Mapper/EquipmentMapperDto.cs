dc9a9fb885da140a97717666500a71c5f3472e7a
lleen <mygoldencode@gmail.com>
2025-11-17 08:34:32 +0500

    AddMapperToEquipment

---
+++ QazaqGeoReports/QazaqGeoReports.Application/Mapper/EquipmentMapperDto.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using QazaqGeoReports.Application.DTOs.EquipmentDtos;
+using QazaqGeoReports.Domain.Entities;
+
+namespace QazaqGeoReports.Application.Mapper;
+
+public class EquipmentMapperDto : Profile
+{
+    public EquipmentMapperDto()
+    {
+        CreateMap<CreateEquipmentDto, Equipment>().ReverseMap();
+        CreateMap<UpdateEquipmentDto, Equipment>().ReverseMap();
+        CreateMap<BaseEquipmentDto, Equipment>().ReverseMap();
+        CreateMap<ListEquipmentViewModel, Equipment>().ReverseMap();
+    }
+}
